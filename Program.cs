f5acbc7c472e6bcdcb763e7e81d3d1a84860f901
Javohir0102 <javohiranvarov96@gmail.com>
2024-06-10 15:38:07 +0500

    Updated Program

--- Program.cs
+++ Program.cs
@@ -1,49 +1,8 @@
 ﻿using System;
-using System.ComponentModel.Design;
-using System.Xml.XPath;
-
-// Console.Write("Enter the speaking score: ");
-// string inputSpeaking = Console.ReadLine();
-// double speaking = Convert.ToDouble(inputSpeaking);
-
-// Console.Write("Enter the listening score: ");
-// string inputListening = Console.ReadLine();
-// double listening = Convert.ToDouble(inputListening);
-
-// Console.Write("Enter the reading score: ");
-// string inputReading = Console.ReadLine();
-// double reading = Convert.ToDouble(inputReading);
-
-// Console.Write("Enter the writing score: ");
-// string inputWriting = Console.ReadLine();
-// double writing = Convert.ToDouble(inputWriting);
-
-// Console.WriteLine(YourOverallScore(speaking, listening, reading, writing));
-
-// var grade = YourOverallScore(speaking, listening, reading, writing) switch 
-// {
-//     >= 4.5 and < 5.5 => "5-Modest",
-//     >= 5.5 and < 6.5 => "6-Component",
-//     >= 6.5 and < 7.5 => "7-Good",
-//     >= 7.5 and < 8.5 => "8-Very good",
-//     >= 8.5 and < 9 => "9-Expert",
-//     _ => "Below or above that range is Invalid"   
-// };
-// ShowPartialMessage(grade);
-
-// static double YourOverallScore(double speaking, double listening, double reading, double writing)
-// {
-//     double overall = (speaking+listening+reading+writing) / 4;
-//     return overall;
-// }
-
-// static void ShowPartialMessage(string message)
-// {
-//     Console.WriteLine(message);
-// }
 
 Console.WriteLine("Welcome to Ielts calculator program");
 char select = '1';
+
 do
 {    
     PrintText("Enter the speaking score: ");
@@ -125,4 +84,3 @@ do
     select = Convert.ToChar(Console.ReadLine());
 
 } while (select == 1);
-
